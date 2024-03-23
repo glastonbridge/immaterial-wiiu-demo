3282cedab815e89f8d7e62f17ae291911836f8ff
halcy <halcy@ARARAGI-KUN>
2024-03-23 15:43:18 +0200

    add a bunch of assets

--- scenes/SceneAssets.cpp
+++ scenes/SceneAssets.cpp
@@ -1,21 +1,5 @@
 #include "SceneAssets.h"
 
-SceneAssets::SceneAssets() {
-    // Load materials
-    materials.push_back(std::make_unique<BoneMaterial>("assets/train_small.png"));
-    materials.push_back(std::make_unique<BoneMaterial>("assets/train_small.png")); // no cushion texture, using train instead (for now)
-    materials.push_back(std::make_unique<BoneMaterial>("assets/house1_small.png"));
-    materials.push_back(std::make_unique<BoneMaterial>("assets/lampshade_small.png"));
-    materials.push_back(std::make_unique<BoneMaterial>("assets/colorgrid.png"));
-
-    // Load objects
-    objects.push_back(LoadObject("assets/train.fbx", NULL, materials[ID_train_mat].get()));
-    objects.push_back(LoadObject("assets/cushion.fbx", NULL, materials[ID_cushion_mat].get())); 
-    objects.push_back(LoadObject("assets/house1.fbx", NULL, materials[ID_house1_mat].get()));
-    objects.push_back(LoadObject("assets/lampshade.fbx", NULL, materials[ID_lampshade_mat].get()));
-    objects.push_back(LoadObject("assets/gizmos.fbx", "Cube", materials[ID_colorgrid_mat].get()));
-};
-
 // Getter for the asset store
 static SceneAssets* globalAssetStore = nullptr;
 SceneAssets* getSceneAssets() {
--- scenes/SceneAssets.h
+++ scenes/SceneAssets.h
@@ -9,22 +9,46 @@ enum objectID : size_t {
     ID_cushion,
     ID_house1,
     ID_lampshade,
-    ID_gizmos
+    ID_gizmos,
+    ID_house2,
+    ID_viaduct,
+    ID_duvet_hills
 };
 
 enum materialID : size_t {
-    ID_train_mat,
-    ID_cushion_mat,
-    ID_house1_mat,
-    ID_lampshade_mat,
-    ID_colorgrid_mat
+    ID_mat_train,
+    ID_mat_duvet,
+    ID_mat_house1,
+    ID_mat_lampshade,
+    ID_mat_colorgrid,
+    ID_mat_house2,
+    ID_mat_viaduct
 };
 
 struct SceneAssets {
     std::vector<std::unique_ptr<SceneObject>> objects;
     std::vector<std::unique_ptr<SceneMaterial>> materials;
 
-    SceneAssets();
+    SceneAssets() {
+        // Load materials
+        materials.push_back(std::make_unique<BoneMaterial>("assets/train_small.png"));
+        materials.push_back(std::make_unique<BoneMaterial>("assets/duvet.png"));
+        materials.push_back(std::make_unique<BoneMaterial>("assets/house1_small.png"));
+        materials.push_back(std::make_unique<BoneMaterial>("assets/lampshade_small.png"));
+        materials.push_back(std::make_unique<BoneMaterial>("assets/colorgrid.png"));
+        materials.push_back(std::make_unique<BoneMaterial>("assets/house2_small.png"));
+        materials.push_back(std::make_unique<BoneMaterial>("assets/viaduct.png"));
+
+        // Load objects
+        objects.push_back(LoadObject("assets/train.fbx", NULL, materials[ID_mat_train].get()));
+        objects.push_back(LoadObject("assets/cushion.fbx", NULL, materials[ID_mat_duvet].get())); 
+        objects.push_back(LoadObject("assets/house1.fbx", NULL, materials[ID_mat_house1].get()));
+        objects.push_back(LoadObject("assets/lampshade.fbx", NULL, materials[ID_mat_lampshade].get()));
+        objects.push_back(LoadObject("assets/gizmos.fbx", "Cube", materials[ID_mat_colorgrid].get()));
+        objects.push_back(LoadObject("assets/house2.fbx", NULL, materials[ID_mat_house2].get()));
+        objects.push_back(LoadObject("assets/viaduct.fbx", NULL, materials[ID_mat_viaduct].get()));
+        objects.push_back(LoadObject("assets/duvet_hills.fbx", NULL, materials[ID_mat_duvet].get()));
+    };
 };
 
 SceneAssets* getSceneAssets();
