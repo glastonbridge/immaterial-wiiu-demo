bcde83daeecefa8b59a02462b5e5fa00372128a0
MrsBeanbag <amy.worthington@glastonbridge.com>
2024-04-05 17:07:02 +0100

    Fix clang-format's silliness

--- util/memory.cpp
+++ util/memory.cpp
@@ -3,7 +3,7 @@
 void swap_memcpy(void *dst, const void *src, uint32_t size) {
   uint32_t *csrc = (uint32_t *)src;
   uint32_t *cdst = (uint32_t *)dst;
-  for (uint32_t i = 0; i<size> > 2; i++) {
+  for (uint32_t i = 0; i < (size >> 2); i++) {
     cdst[i] = SWAP_32(csrc[i]);
   }
 }
